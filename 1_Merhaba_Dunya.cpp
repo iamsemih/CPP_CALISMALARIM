467fcb96cb25fe99c9c2c67bf6abca8307974d6b
Semih Sahin <ckl.mhnds@gmail.com>
2017-12-10 23:57:50 +0300

    MerhabaDunya

---
+++ 1_Merhaba_Dunya.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>	// getch(); fonksiyonu icin kutuphane
+// SEMIH
+using namespace std;
+
+int main()
+{
+	cout << "Merhaba Dunya";	// Ekrana yazilacak olan yazi
+	getch();	// getch fonksiyonu enter tusuna basilana kadar bekler system("pause") islevini de gorur bir nebze
+	return 0;
+}
